02044e17794345670ce707388b39e8077ab7a1b9
akshit-adeshra <jwick4298@gmail.com>
2021-05-17 20:12:34 +0530

    GET, POST, PUT, DELETE Checklist methods added

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views import TestAPIView, CheckListsAPIView
+from core.views import TestAPIView, CheckListsAPIView, CheckListAPIView
 
 
 urlpatterns = [
     path('', TestAPIView.as_view()),
     path('api/checklists/', CheckListsAPIView.as_view()),
+    path('api/checklist/<int:pk>/', CheckListAPIView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
+from django.http.response import Http404
 from django.shortcuts import render
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+
 from core.models import CheckList
 from core.serializers import CheckListSerializer
 
@@ -22,4 +26,44 @@ class CheckListsAPIView(APIView):
         # fetching the serialized data from the serializer
         serialized_data = serializer.data
 
-        return Response(serialized_data)
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            serialized_data = serializer.data
+            return Response(serialized_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CheckListAPIView(APIView):
+    serializer_class = CheckListSerializer
+
+    def get_object(self, pk):
+        try:
+            return CheckList.objects.get(pk=pk)
+        except CheckList.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        serializer = self.serializer_class(self.get_object(pk))
+        serialized_data = serializer.data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        checklist = self.get_object(pk)
+
+        # just as in saving form data, we need to associate this data with a particular instance of the model,
+        # so model is also passed as the first argument
+        serializer = self.serializer_class(checklist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            serialized_data = serializer.data
+            return Response(serialized_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        checklist = self.get_object(pk)
+        checklist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
