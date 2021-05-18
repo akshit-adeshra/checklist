2099c0778d168480149a80fa52499226fd8cc2b1
akshit-adeshra <jwick4298@gmail.com>
2021-05-18 14:16:21 +0530

    Added Documentation for all the APIs

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from core.views import TestAPIView, CheckListsAPIView, CheckListAPIView, CheckListItemCreateAPIView, CheckListItemAPIView
+from core.views import CheckListsAPIView, CheckListAPIView, CheckListItemCreateAPIView, CheckListItemAPIView
 
 
 urlpatterns = [
-    path('', TestAPIView.as_view()),
     path('api/checklists/', CheckListsAPIView.as_view()),
     path('api/checklist/<int:pk>/', CheckListAPIView.as_view()),
     path('api/checklistItem/create/', CheckListItemCreateAPIView.as_view()),
