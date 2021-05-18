565d417f1bd142992256c9668249d1a64dc6ddd6
akshit-adeshra <jwick4298@gmail.com>
2021-05-18 12:28:47 +0530

    Displayed all the CheckListItems of each Checklist in a nested format

--- core/serializers.py
+++ core/serializers.py
@@ -2,13 +2,19 @@ from rest_framework import serializers
 from core.models import CheckList, CheckListItem
 
 
-class CheckListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CheckList
-        fields = '__all__'
-
-
 class CheckListItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CheckListItem
+        fields = '__all__'
+
+
+class CheckListSerializer(serializers.ModelSerializer):
+    # here we are specifying a new field named items so as to we can see all the items of a checklist in its nested form
+    items = CheckListItemSerializer(source='checklistitem_set', many=True, read_only=True)
+    # above we have passed the foreign_key (that we've already defined along with the CheckListItem model) in the source
+    # parameter, many=True is specified as there can be multiple CheckList items for a single checklist, and
+    # read_only=True is specified so user can not create new item while reading all the checklists.
+
+    class Meta:
+        model = CheckList
         fields = '__all__'
