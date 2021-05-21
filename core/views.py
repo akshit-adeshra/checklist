70ec5b46e702afe5fb923745fa45153ddde7d8db
akshit-adeshra <jwick4298@gmail.com>
2021-05-21 11:55:49 +0530

    Refactored the code to Generic API Views of DRF

--- core/views.py
+++ core/views.py
@@ -1,117 +1,54 @@
-from django.http.response import Http404
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    CreateAPIView,
+)
 
 from core.permissions import IsOwner
 from core.models import CheckList, CheckListItem
 from core.serializers import CheckListSerializer, CheckListItemSerializer
 
 
-class CheckListsAPIView(APIView):
+class CheckListsAPIView(ListCreateAPIView):
+    """
+    Listing, Creation
+    """
     serializer_class = CheckListSerializer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def get(self, request, format=None):
-        data = CheckList.objects.filter(user=request.user)
-
-        # instantiating the serializer class, & giving it the queryset data as parameter
-        serializer = self.serializer_class(data, many=True)
-
-        # fetching the serialized data from the serializer
-        serialized_data = serializer.data
-
-        return Response(serialized_data, status=status.HTTP_200_OK)
-
-    def post(self, request, format=None):
-        # context var is passed coz APIView doesn't by default pass request object
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        queryset = CheckList.objects.filter(user=self.request.user)
+        return queryset
 
 
-class CheckListAPIView(APIView):
+class CheckListAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Update, Destroy
+    """
     serializer_class = CheckListSerializer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def get_object(self, pk):
-        try:
-            obj = CheckList.objects.get(pk=pk)
-            self.check_object_permissions(self.request, obj)
-            return obj
-        except CheckList.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        serializer = self.serializer_class(self.get_object(pk))
-        serialized_data = serializer.data
-        return Response(serialized_data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk, format=None):
-        checklist = self.get_object(pk)
-
-        # just as in saving form data, we need to associate this data with a particular instance of the model,
-        # so model is also passed as the first argument
-        serializer = self.serializer_class(checklist, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        checklist = self.get_object(pk)
-        checklist.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        queryset = CheckList.objects.filter(user=self.request.user)
+        return queryset
 
 
-class CheckListItemCreateAPIView(APIView):
+class CheckListItemCreateAPIView(CreateAPIView):
+    """
+    Creation
+    """
     serializer_class = CheckListItemSerializer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def post(self, request, format=None):
-        # context var is passed coz APIView doesn't by default pass request object
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class CheckListItemAPIView(APIView):
+class CheckListItemAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Update, Destroy
+    """
     serializer_class = CheckListItemSerializer
     permission_classes = [IsAuthenticated, IsOwner]
 
-    def get_object(self, pk):
-        try:
-            obj = CheckListItem.objects.get(pk=pk)
-            self.check_object_permissions(self.request, obj)
-            return obj
-        except CheckListItem.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        checklist_item = self.get_object(pk)
-        serializer = self.serializer_class(checklist_item)
-        serialized_data = serializer.data
-        return Response(serialized_data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk, format=None):
-        checklist_item = self.get_object(pk)
-        serializer = self.serializer_class(checklist_item, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        checklist_item = self.get_object(pk)
-        checklist_item.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        queryset = CheckListItem.objects.filter(user=self.request.user)
+        return queryset
